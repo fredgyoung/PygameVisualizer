b4746e027510708bac7f149e75d8387631174aea
Fred Young <fredgyoung@gmail.com>
2020-09-22 21:31:59 -0400

    added some movement

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ pygame.display.set_caption("Example")
 
 cards = []
 
+def swap_cards(i, j):
+    cards[i].swap_position(cards[j].x)
+    cards[j].swap_position(cards[i].x)
+
 class Card():
     def __init__(self, x, y, value):
         self.x = x
@@ -29,6 +33,17 @@ class Card():
         self.height = 80
         self.value = value
         self.destinations = []
+
+    def swap_position(self, position):
+        self.destinations.append((self.x, self.y-200))
+        self.destinations.append((position, self.y-200))
+        self.destinations.append((position, self.y))
+
+    def shift_right(self):
+        self.destinations.append((self.x+100, self.y))
+
+    def shift_left(self):
+        self.destinations.append((self.x-100, self.y))
 
     def update(self):
         if self.destinations:
@@ -46,33 +61,22 @@ class Card():
 
     def draw(self, surface):
         pygame.draw.rect(surface, BLACK, (self.x, self.y, self.width, self.height), 1)
-
-        # load font, prepare values
         font = pygame.font.Font(None, 50)
         text = str(self.value)
         size = font.size(text)
-
-        # no AA, no transparancy, normal
         ren = font.render(text, 0, BLACK)
         surface.blit(ren, (self.x + 30, self.y + 20))
-
-'''
-def move_card(index, destination_x, destination_y):
-    if cards[index].x < destination_x: cards[index].x += 1
-    elif cards[index].x > destination_x: cards[index].x -= 1
-    if cards[index].y < destination_y: cards[index].y += 1
-    elif cards[index].y > destination_y: cards[index].y -= 1
-'''
 
 for x in range(10):
     card = Card(x*100+100, 400, x)
     cards.append(card)
 
+'''
 cards[8].value = 1
 cards[1].value = 8
-
 cards[8].destinations.extend([(900, 200), (200, 200), (200, 400)])
 cards[1].destinations.extend([(200, 200), (900, 200), (900, 400)])
+'''
 
 # Beginning Game Loop
 while True:
@@ -80,6 +84,17 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                for card in cards:
+                    card.shift_right()
+            if event.key == pygame.K_LEFT:
+                for card in cards:
+                    card.shift_left()
+            if event.key == pygame.K_UP:
+                swap_cards(1, 8)
+                #cards[1].swap_position(8)
+                #cards[8].swap_position(1)
 
     DISPLAYSURF.fill(WHITE)
 
